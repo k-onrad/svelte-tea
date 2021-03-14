ad80c3ed1909a871368e901a2bf2c8fe1207574d
Gustavo Konrad <g.konrad@outlook.com>
2021-03-13 21:11:14 -0300

    added terser, done w/ lib i think?

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,85 @@
-console.log('Hello world!')
+import { getContext, setContext } from 'svelte'
+import { writable, derived } from 'svelte/store'
+import type { Readable } from 'svelte/store'
+
+const MODEL = typeof Symbol !== 'undefined' ? Symbol('model') : '@model'
+
+export type Value<Model> = Model[keyof Model]
+
+export type ModelNode<Model> = Value<Model> | Model
+
+export type Dispatch<Message> = (msg: Message) => void
+
+export type ModelAPI<Model, Message> = [Readable<ModelNode<Model>>, Dispatch<Message>]
+
+export type UpdateFunction<Model, Message> = (msg: Message) => (state: Model) => Model
+
+export type Middleware<Model, Message> = ([model, dispatch]: ModelAPI<Model, Message>) => (next: Dispatch<Message>) => (msg: Message) => void
+
+function isObject<T, S> (x: T | S): x is T {
+  return typeof x === 'object' && x !== null
+}
+
+function hasProp<T> (obj: T, x: string | number | symbol): x is keyof T {
+  return x in obj
+}
+
+function reduce<T, S> (reducer: (a: T, cur: S) => T, init: T, iterable: S[]): T {
+  return iterable.reduce(reducer, init)
+}
+
+export const useModel = <Model, Message> (...path: string[]): ModelAPI<Model, Message> => {
+  const [model, dispatch]: ModelAPI<Model, Message> = getContext(MODEL)
+
+  if (model == null) {
+    throw new Error('Context not found. Please ensure you provide the model using "provideModel" function')
+  }
+
+  const { subscribe } = derived(
+    model,
+    ($model: ModelNode<Model>): ModelNode<Model> =>
+      reduce<ModelNode<Model>, string>((acc: ModelNode<Model>, cur: string) => isObject<Model, Value<Model>>(acc) && hasProp(acc, cur) ? acc[cur] : acc, $model, path),
+  )
+
+  return [{ subscribe }, dispatch]
+}
+
+export const provideModel = <Model, Message> ([model, dispatch]: ModelAPI<Model, Message>): void => {
+  setContext(MODEL, [model, dispatch])
+}
+
+export const createModel = <Model, Message> (updater: UpdateFunction<Model, Message>) => (initialModel: Model): ModelAPI<Model, Message> => {
+  const { subscribe, update } = writable(initialModel)
+
+  const dispatch = (msg: Message): void => {
+    update(updater(msg))
+  }
+
+  return [{ subscribe }, dispatch]
+}
+
+export const withMiddleware = <Model, Message> (...middlewares: Array<Middleware<Model, Message>>) => (updater: UpdateFunction<Model, Message>) => (initialModel: Model): ModelAPI<Model, Message> => {
+  const [model, oldDispatch] = createModel(updater)(initialModel)
+
+  if (!Array.isArray(middlewares) || middlewares.length === 0) return [model, oldDispatch]
+
+  // some trickery to make every middleware call to 'dispatch'
+  // go through the whole middleware chain again
+  let dispatch = (_msg: Message): void => {
+    throw new Error(
+      'Dispatching while constructing your middleware is not allowed. ' +
+      'Other middleware would not be applied to this dispatch.',
+    )
+  }
+
+  const middlewareAPI: ModelAPI<Model, Message> = [
+    model,
+    (msg) => dispatch(msg),
+  ]
+
+  dispatch = middlewares
+    .map((middleware) => middleware(middlewareAPI))
+    .reduce((next, middleware) => middleware(next), oldDispatch)
+
+  return [model, dispatch]
+}
