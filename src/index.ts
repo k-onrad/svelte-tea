7270989cec87399766d2ffeef7533cfa01ec4db9
Gustavo Konrad <g.konrad@outlook.com>
2021-03-15 18:49:06 -0300

    changing middleware API to integrate with redux middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { getContext, setContext } from 'svelte'
-import { writable, derived } from 'svelte/store'
+import { writable, derived, get } from 'svelte/store'
 import type { Readable } from 'svelte/store'
 
 /* Reserved Constant */
@@ -12,11 +12,13 @@ export type ModelNode<Model> = Value<Model> | Model
 
 export type Dispatch<Message> = (msg: Message) => void
 
-export type ModelAPI<Model, Message> = [Readable<ModelNode<Model>>, Dispatch<Message>]
+export type ModelAPI<Model, Message> = Readable<ModelNode<Model>> & {dispatch: Dispatch<Message>}
+
+export interface MiddleWareAPI<Model, Message> {getState: () => ModelNode<Model>, dispatch: Dispatch<Message>}
 
 export type UpdateFunction<Model, Message> = (msg: Message) => (model: Model) => Model
 
-export type Middleware<Model, Message> = ([model, dispatch]: ModelAPI<Model, Message>) => (next: Dispatch<Message>) => (msg: Message) => void
+export type Middleware<Model, Message> = (model: MiddleWareAPI<Model, Message>) => (next: Dispatch<Message>) => (msg: Message) => void
 
 /* TypeGuards */
 function isObjNotProp<T> (x: T | T[keyof T]): x is T {
@@ -35,13 +37,13 @@ export const createModel = <Model, Message> (update: UpdateFunction<Model, Messa
     updateStore(update(msg))
   }
 
-  return [{ subscribe }, dispatch]
+  return { subscribe, dispatch }
 }
 
 export const withMiddleware = <Model, Message> (...middlewares: Array<Middleware<Model, Message>>) => (update: UpdateFunction<Model, Message>) => (init: Model): ModelAPI<Model, Message> => {
-  const [model, oldDispatch] = createModel<Model, Message>(update)(init)
+  const model = createModel<Model, Message>(update)(init)
 
-  if (!Array.isArray(middlewares) || middlewares.length === 0) return [model, oldDispatch]
+  if (!Array.isArray(middlewares) || middlewares.length === 0) return model
 
   // Some trickery to make every middleware call to 'dispatch'
   // go through the whole middleware chain again
@@ -52,30 +54,30 @@ export const withMiddleware = <Model, Message> (...middlewares: Array<Middleware
     )
   }
 
-  const middlewareAPI: ModelAPI<Model, Message> = [
-    model,
-    (msg) => dispatch(msg),
-  ]
+  const middlewareAPI: MiddleWareAPI<Model, Message> = {
+    getState: () => get(model),
+    dispatch: (msg) => dispatch(msg),
+  }
 
   dispatch = middlewares
     .map((middleware) => middleware(middlewareAPI))
-    .reduce((next, middleware) => middleware(next), oldDispatch)
+    .reduce((next, middleware) => middleware(next), model.dispatch)
 
-  return [model, dispatch]
+  return { ...model, dispatch }
 }
 
-export const provideModel = <Model, Message> ([model, dispatch]: ModelAPI<Model, Message>): void => {
-  setContext(MODELKEY, [model, dispatch])
+export const provideModel = <Model, Message> (model: ModelAPI<Model, Message>): void => {
+  setContext(MODELKEY, model)
 }
 
-export const useModel = <Model, Message> (...path: string[]): ModelAPI<Model, Message> => {
-  const [model, dispatch]: ModelAPI<Model, Message> = getContext(MODELKEY)
+export const useModel = <Model, Message> (...path: string[]): [Readable<ModelNode<Model>>, Dispatch<Message>] => {
+  const model: ModelAPI<Model, Message> = getContext(MODELKEY)
 
   if (model == null) {
     throw new Error('Context not found. Please ensure you provide the model using "provideModel" function')
   }
 
-  const { subscribe } = derived(
+  const modelNode = derived(
     model,
     ($model: ModelNode<Model>): ModelNode<Model> => {
       const reducer = (acc: ModelNode<Model>, cur: string): ModelNode<Model> => {
@@ -86,5 +88,5 @@ export const useModel = <Model, Message> (...path: string[]): ModelAPI<Model, Me
     },
   )
 
-  return [{ subscribe }, dispatch]
+  return [modelNode, model.dispatch]
 }
